0e81be50204542acf6418668243f9446b18e98c5
akhil <akhilpraghav@gmail.com>
2023-07-14 23:45:53 +0530

    title got from array to title function and created ui

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+
+//grocery array 
+//---------------------------------------------------------------------------------
+const groceries =[
+    {
+        product:"milk",quantity:1,price:1.5
+    },
+    {
+        product:"banana",quanity:2,price:2.5
+    }
+];
+
+
+//getting title
+//--------------------------------------------------------------------------------------
+const getTitles = (groceries)=>{
+ return Object.keys(groceries[0]) 
+}
+
+
+//UI creation
+//---------------------------------------------------------------------------------------
+const createUI = ()=>{
+    const groceriesParenEl = document.querySelector('.groceries');
+   const title= getTitles(groceries);
+   console.log(title)
+   title.map((title)=>{
+    const capsTitle =title.charAt().toUpperCase() + title.slice(1)
+    const groceriesListEl =document.createElement('li');
+    groceriesListEl.innerHTML =capsTitle;
+    groceriesParenEl.appendChild(groceriesListEl);
+   })
+  
+}
+createUI()
+
+//total price of grocery
+//--------------------------------------------------------------------------------------
 let getTotalPrize = (groceries)=>{
-    console.log(groceries)
+    //console.log(groceries)
     let totalCost = groceries.map((item)=>{
         return Object.values(item).slice(-2).reduce((acc,num)=>acc*num)
     }).reduce((acc,num)=>acc+num)
@@ -8,11 +46,4 @@ let getTotalPrize = (groceries)=>{
 
 
 
-let result = getTotalPrize([
-    {
-        product:"milk",quantity:1,price:1.5
-    },
-    {
-        product:"banana",quanity:2,price:2.5
-    }
-])
+ getTotalPrize(groceries)
