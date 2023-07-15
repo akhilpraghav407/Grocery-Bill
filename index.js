09f9c0463576d3d917f775a059c2ab1ea0ed308f
akhil <akhilpraghav@gmail.com>
2023-07-15 14:32:11 +0530

    getValue fnctn nd totalCost fnctn created printed the total cost in UI

--- index.js
+++ index.js
@@ -11,12 +11,34 @@ const groceries =[
 ];
 
 
+
+
+
+
+
+
 //getting title
 //--------------------------------------------------------------------------------------
 const getTitles = (groceries)=>{
  return Object.keys(groceries[0]) 
 }
-
+//getting values
+//--------------------------------------------------------------------------------------
+const getValues = (groceries)=>{
+    return groceries.map((groceryIem)=>{
+       return Object.values(groceryIem)
+    })
+}
+//total price of grocery
+//--------------------------------------------------------------------------------------
+let getTotalPrize = (groceries)=>{
+    //console.log(groceries)
+    let totalCost = groceries.map((item)=>{
+        return Object.values(item).slice(-2).reduce((acc,num)=>acc*num)
+    }).reduce((acc,num)=>acc+num)
+    return totalCost
+   
+}
 
 //UI creation
 //---------------------------------------------------------------------------------------
@@ -27,23 +49,41 @@ const createUI = ()=>{
    title.map((title)=>{
     const capsTitle =title.charAt().toUpperCase() + title.slice(1)
     const groceriesListEl =document.createElement('li');
+    groceriesListEl.classList.add('title');
     groceriesListEl.innerHTML =capsTitle;
     groceriesParenEl.appendChild(groceriesListEl);
-   })
+   });
+
+// get the values
+  let groceryValues =getValues(groceries);
+  //console.log(groceryValues);
+  groceryValues.map((item)=>{
+    item.forEach((value)=>{
+       // console.log(value)
+       const groceriesListEl =document.createElement('li');
+       groceriesListEl.innerHTML = value;
+       groceriesParenEl.appendChild(groceriesListEl);
+
+
+    })
+  })
+
+
+  // total price
+  const totalCost = getTotalPrize(groceries)
+  console.log(totalCost)
+  const sectionEl = document.querySelector('.section-subtitle');
+  const spanEl = document.createElement('span');
+  spanEl.setAttribute('id','title')
+  spanEl.innerHTML = totalCost;
+  sectionEl.appendChild(spanEl)
   
+
 }
-createUI()
-
-//total price of grocery
-//--------------------------------------------------------------------------------------
-let getTotalPrize = (groceries)=>{
-    //console.log(groceries)
-    let totalCost = groceries.map((item)=>{
-        return Object.values(item).slice(-2).reduce((acc,num)=>acc*num)
-    }).reduce((acc,num)=>acc+num)
-    console.log(totalCost)
-}
+createUI();
 
 
 
- getTotalPrize(groceries)
+
+
+
